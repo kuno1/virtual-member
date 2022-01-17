0218bca0f0a017fa6d0c7ea2360b9eb248c6e710
fumikito <guy@hametuha.com>
2022-01-17 17:58:25 +0900

    Fix getter for available post types.

--- src/Kunoichi/VirtualMember/PostType.php
+++ src/Kunoichi/VirtualMember/PostType.php
@@ -154,10 +154,9 @@ class PostType extends Singleton {
 	public static function available_post_types() {
 		$instance = self::get_instance();
 		if ( is_null( $instance->post_types_having_author ) ) {
-			return get_option( 'kvm_available_post_types', [] );
-		} else {
-			return $instance->post_types_having_author;
+			$instance->post_types_having_author = get_option( 'kvm_available_post_types', [] );
 		}
+		return $instance->post_types_having_author;
 	}
 
 	/**
