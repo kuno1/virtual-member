61dadd2eb821c7261c67ad9afcff96675a5848b8
fumikito <guy@hametuha.com>
2024-04-23 12:02:00 +0900

    closes #7

--- src/Kunoichi/VirtualMember/Ui/PublicScreen.php
+++ src/Kunoichi/VirtualMember/Ui/PublicScreen.php
@@ -68,11 +68,18 @@ class PublicScreen extends Singleton {
 	 * @return string
 	 */
 	public function hook_author_archive( $link, $author_id, $author_nicename ) {
-		if ( (int) get_post()->post_author !== $author_id ) {
+		if ( is_admin() || ! in_the_loop() ) {
+			return $link;
+		}
+		$post = get_post();
+		if ( ! $post ) {
+			return $link;
+		}
+		if ( (int) $post->post_author !== $author_id ) {
 			// This is not in loop.
 			return $link;
 		}
-		$author_archive = $this->get_author_archive();
+		$author_archive = $this->get_author_archive( $post );
 		if ( $author_archive ) {
 			$link = $author_archive;
 		}
